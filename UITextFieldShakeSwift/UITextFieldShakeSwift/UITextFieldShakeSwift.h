a5a235815ed3c9669704eb430414ef90ebf60eb6
King-Wizard <king.wizard.contact@gmail.com>
2015-08-07 02:47:07 +0200

    Second commit.

---
+++ UITextFieldShakeSwift/UITextFieldShakeSwift/UITextFieldShakeSwift.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITextFieldShakeSwift.
+FOUNDATION_EXPORT double UITextFieldShakeSwiftVersionNumber;
+
+//! Project version string for UITextFieldShakeSwift.
+FOUNDATION_EXPORT const unsigned char UITextFieldShakeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITextFieldShakeSwift/PublicHeader.h>
+
+
---
+++ UITextFieldShakeSwift/UITextFieldShakeSwiftDemo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  UITextFieldShakeSwiftDemo
+//
+//  Initially created by Andrea Mazzini (using Objective-C) on 08/02/14.
+//  Translated from Objective-C to Swift by King-Wizard
+//  Copyright (c) 2015 Xylon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
